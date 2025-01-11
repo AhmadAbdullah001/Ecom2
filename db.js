7b4815de17df634804e4ce8d6a9227b5c29968c5
Abdullah <ahmadhashmi1304@gmail.com>
2025-01-12 01:49:25 +0530

    Commit

---
+++ db.js
@@ -0,0 +1,6 @@
+const mongoose=require('mongoose')
+const mongoURI = "mongodb+srv://ahmadhashmi1304:xt2kut17@cluster0.oe36p.mongodb.net/EcomDatabase";
+async function connectToMongo() {
+    await mongoose.connect(mongoURI).then(()=>{console.log("connected Successfully")}).catch((err)=>{console.log(err)})
+}
+module.exports=connectToMongo;
